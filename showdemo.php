6bd9168279c38a3f722ad53e853c67d76fb9e5dd
tongswitss <tongswitss@gmail.com>
2021-12-02 16:33:11 +0700

    aa

--- showdemo.php
+++ showdemo.php
@@ -7,6 +7,7 @@ $dbname = "it64309010006";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
+mysqli_set_charset($conn,"utf8");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
